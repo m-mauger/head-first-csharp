b508ca69b415c1ce5a90ed8495f53a35add60851
m-mauger <mm98business@gmail.com>
2023-08-06 17:18:32 -0500

    added initial book exercises

---
+++ chapter-1/MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TryAnIf();			
+			TryAnIfElse();
+			TrySomeLoops();
+		}
+
+		private static void TryAnIf()
+		{
+			int someValue = 4;
+			string name = "Bobbo Jr.";
+			if ((someValue == 3) && (name == "Joe"))
+            {
+				Console.WriteLine("x is 3 and the name is Joe");
+            }
+			Console.WriteLine("this line runs no matter what");
+		}
+
+		private static void TryAnIfElse()
+        {
+			int x = 5;
+			if (x == 10)
+            {
+				Console.WriteLine("x must be 10");
+            }
+            else
+            {
+				Console.WriteLine("x isn't 10");
+            }
+        }
+
+        private static void TrySomeLoops()
+        {
+			int count = 0;
+            while (count < 10)
+            {
+				count = count + 1;
+            }
+            for (int i = 0; i < 5; i++)
+            {
+				count = count - 1;
+            }
+			Console.WriteLine("The answer is " + count);
+        }
+
+        private static void LoopExamples()
+        {
+			// Loop #3
+			int p = 2;
+			for(int q = 2; q < 32; q = q * 2)
+            {
+				while(p < q)
+                {
+					p = p * 2;
+                }
+				q = p - q;
+            }
+        }
+
+        private static void OperatorExamples()
+        {
+			// This statement declares a variable and sets it to 3
+			int width = 3;
+
+			// The ++ operator increments a variable (adds 1 to it)
+			width++;
+
+			// Declare two more int variables to hold numbers and
+			// use the + and * operators to add and multiply values
+			int height = 2 + 4;
+			int area = width * height;
+			Console.WriteLine(area);
+
+            while (area < 20)
+            {
+
+				height++;
+				area = width * height;
+
+            }
+
+            do
+            {
+
+				width--;
+				area = width * height;
+
+            } while (area > 25);
+
+			// The next two statements declare string variables
+			// and use + to concatenate them (join them together)
+			string result = "The area";
+			result = result + " is " + area;
+			Console.WriteLine(result);
+
+			// A Boolean variable is either true or false
+			bool truthValue = true;
+			Console.WriteLine(truthValue);
+        }
+    }
+}
